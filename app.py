b091c19377138d6720af50f9133662140a4d17cd
SANJU KUMAR <mighty_sanju@SANJUs-MacBook-Air-2.local>
2024-07-16 05:34:40 -0500

    doc added

--- app.py
+++ app.py
@@ -46,5 +46,6 @@ def ask_bot():
     return jsonify({'answer': answer})
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
